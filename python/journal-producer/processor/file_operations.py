a3ecee179edc64e0a712f3fddfea58e35de2b887
samssi <samir.hamadi@gmail.com>
2019-02-03 22:23:32 +0200

    Initial Kafka placeholder

--- python/journal-producer/processor/file_operations.py
+++ python/journal-producer/processor/file_operations.py
@@ -1,7 +1,14 @@
+import json
+
 def stream_file_as_json(file):
     with open(file, 'r') as file:
         try:
             for line in file:
-                print(line)
+                parsed = json.loads(line)
+                json_string = json.dumps(parsed)
+                send_to_kafka(json_string)
         finally:
             file.close()
+
+def send_to_kafka(json_string):
+    print(json_string)
