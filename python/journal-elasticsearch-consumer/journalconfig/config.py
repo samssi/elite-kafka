558fcc3784ad060fca704e7206d2df9806153673
samssi <samir.hamadi@gmail.com>
2019-02-04 21:21:24 +0200

    Add elasticsearch settings

--- python/journal-elasticsearch-consumer/journalconfig/config.py
+++ python/journal-elasticsearch-consumer/journalconfig/config.py
@@ -7,3 +7,7 @@ default_file = 'default.ini'
 def read_kafka_config():
     parser.read(paths.settings_file(default_file))
     return parser['kafka']
+
+def read_elasticsearch_config():
+    parser.read(paths.settings_file(default_file))
+    return parser['elasticsearch']
--- python/journal-elasticsearch-consumer/processor/journal_consumer.py
+++ python/journal-elasticsearch-consumer/processor/journal_consumer.py
@@ -3,15 +3,15 @@ from journalconfig import config
 from client import elasticsearch_client
 import json
 
-broker_uri = config.read_kafka_config()['broker_uri']
-
 class JournalConsumer:
     def __init__(self):
-        pass
+        self.broker_uri = config.read_kafka_config()['broker_uri']
+        self.elasticsearch_url = config.read_elasticsearch_config()['elasticsearch_url']
+        self.elasticsearch_journal_index = config.read_elasticsearch_config()['elasticsearch_journal_index']
 
     def start(self):
         print()
-        consumer = KafkaConsumer(bootstrap_servers=[broker_uri],
+        consumer = KafkaConsumer(bootstrap_servers=[self.broker_uri],
                                  auto_offset_reset='earliest',
                                  consumer_timeout_ms=1000)
         
@@ -19,6 +19,6 @@ class JournalConsumer:
 
         for message in consumer:
             data = json.loads(message.value)
-            elasticsearch_client.post("http://localhost:9200/elitejournal/_doc", data)
+            elasticsearch_client.post(f'{self.elasticsearch_url}/{self.elasticsearch_journal_index}/_doc', data)
 
         consumer.close()
