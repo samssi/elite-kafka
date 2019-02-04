5f09e5dd4b7ff2f0ee7238dacf0355b51e3b1570
samssi <samir.hamadi@gmail.com>
2019-02-04 21:47:28 +0200

    Make consumer loop and break when interrupted by keyboard

--- python/journal-elasticsearch-consumer/processor/journal_consumer.py
+++ python/journal-elasticsearch-consumer/processor/journal_consumer.py
@@ -2,23 +2,32 @@ from kafka import KafkaConsumer
 from journalconfig import config
 from client import elasticsearch_client
 import json
+import time
 
 class JournalConsumer:
     def __init__(self):
         self.broker_uri = config.read_kafka_config()['broker_uri']
         self.elasticsearch_url = config.read_elasticsearch_config()['elasticsearch_url']
         self.elasticsearch_journal_index = config.read_elasticsearch_config()['elasticsearch_journal_index']
-
-    def start(self):
-        print()
-        consumer = KafkaConsumer(bootstrap_servers=[self.broker_uri],
+        self.consumer = KafkaConsumer(bootstrap_servers=[self.broker_uri],
                                  auto_offset_reset='earliest',
                                  consumer_timeout_ms=1000)
         
-        consumer.subscribe(['elite-journal'])
 
-        for message in consumer:
-            data = json.loads(message.value)
-            elasticsearch_client.post(f'{self.elasticsearch_url}/{self.elasticsearch_journal_index}/_doc', data)
+    def start(self):
+        self.consumer.subscribe(['elite-journal'])
 
-        consumer.close()
+        try:
+            while True:
+                for message in self.consumer:
+                    print("Consuming messages...")
+                    data = json.loads(message.value)
+                    response_status_code = elasticsearch_client.post(f'{self.elasticsearch_url}/{self.elasticsearch_journal_index}/_doc', data).status_code
+                    print(f'{message}: {response_status_code}')
+
+                time.sleep(5)
+
+        except KeyboardInterrupt:
+            self.consumer.close()
+            print('Stopped consumer...')
+        
